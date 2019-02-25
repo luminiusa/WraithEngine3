921e2c5c2d862285678dae1656f3f967bdf017f6
Paul <griesser@kth.se>
2019-02-25 15:41:04 +0100

    #3 add comment to the test in ObjectPoolTest

--- WraithEngine/src/test/java/util_handling/ObjectPoolTest.java
+++ WraithEngine/src/test/java/util_handling/ObjectPoolTest.java
@@ -8,27 +8,32 @@ import net.whg.we.utils.ObjectPool;
 import net.whg.we.utils.logging.LogProperty;
 
 public class ObjectPoolTest {
-    
+    /**
+     * Test that putting and getting from the pool work properly.
+     */
     @Test
     public void putAndGet() {
-        
+
         ObjectPool<LogProperty> pool = new ObjectPool<LogProperty>() {
             @Override
             protected LogProperty build() {
                 return new LogProperty();
             }
         };
-        
+
         LogProperty lp1 = new LogProperty();
         LogProperty lp2 = new LogProperty();
-        
-        
-        
+
+
+
         pool.put(lp1);
         pool.put(lp2);
         assertEquals(lp1,pool.get());
     }
-    
+
+    /**
+     * Test that getting from an empty pool returns a newly built object.
+     */
     @Test
     public void getFromEmptyPool() {
 
@@ -39,8 +44,8 @@ public class ObjectPoolTest {
                 return result.setMessage("default message");
             }
         };
-        
+
         assertEquals(pool.get().getMessage(),"default message");
     }
-    
+
 }
